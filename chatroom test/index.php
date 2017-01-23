adb419d2bbac3c218212df4b673c6ca21455d7be
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-01-23 13:50:27 +0000

    retest

--- chatroom test/index.php
+++ chatroom test/index.php
@@ -4,45 +4,15 @@
 <title>Chat - Customer Module</title>
 <link type="text/css" rel="stylesheet" href="style.css" />
 </head>
-<?
-session_start();
  
-function loginForm(){
-    echo'
-    <div id="loginform">
-    <form action="index.php" method="post">
-        <p>Please enter your name to continue:</p>
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" />
-        <input type="submit" name="enter" id="enter" value="Enter" />
-    </form>
-    </div>
-    ';
-}
- 
-?>
-<?php
-if(!isset($_SESSION['name'])){
-    loginForm();
-}
-else{
-?>
 <div id="wrapper">
     <div id="menu">
-        <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
+        <p class="welcome">Welcome, <b></b></p>
         <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
         <div style="clear:both"></div>
-    </div>    
-    <div id="chatbox">
-    <?php
-    if(file_exists("log.html") && filesize("log.html") > 0){
-    $handle = fopen("log.html", "r");
-    $contents = fread($handle, filesize("log.html"));
-    fclose($handle);
+    </div>
      
-    echo $contents;
-}
-?></div>
+    <div id="chatbox"></div>
      
     <form name="message" action="">
         <input name="usermsg" type="text" id="usermsg" size="63" />
@@ -53,30 +23,8 @@ else{
 <script type="text/javascript">
 // jQuery Document
 $(document).ready(function(){
-//If user wants to end session
-    $("#exit").click(function(){
-        var exit = confirm("Are you sure you want to end the session?");
-        if(exit==true){window.location = 'index.php?logout=true';}      
-    });
-    //If user submits the form
-    $("#submitmsg").click(function(){   
-        var clientmsg = $("#usermsg").val();
-        $.post("post.php", {text: clientmsg});              
-        $("#usermsg").attr("value", "");
-        return false;
-    });
+ 
 });
 </script>
-<?php
-if(isset($_GET['logout'])){ 
-     
-    //Simple exit message
-    $fp = fopen("log.html", 'a');
-    fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
-    fclose($fp);
-     
-    session_destroy();
-    header("Location: index.php"); //Redirect the user
-}
-?>
+</body>
 </html>
