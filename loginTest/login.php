e8ab024c354c94320ac1882324cd702bcfc9b17b
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-01-23 21:39:33 +0000

    come on...

--- loginTest/login.php
+++ loginTest/login.php
@@ -55,7 +55,7 @@
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
       
-      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $sql = "SELECT Userid FROM users WHERE Username = '$myusername' and Userpass = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
