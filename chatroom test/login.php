615784471ea2f71a8233078633ac61fefdbd91ab
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-01-23 15:51:57 +0000

    test test

--- chatroom test/login.php
+++ chatroom test/login.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Chat - Customer Module</title>
+<link type="text/css" rel="stylesheet" href="style.css" />
+</head>
+<body>
  <?
 session_start();
  
@@ -23,3 +30,5 @@ if(isset($_POST['enter'])){
     }
 }
 ?>
+</body>
+</html>
