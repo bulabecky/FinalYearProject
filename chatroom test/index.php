7720b1da9d243799e1f68bb3223682b037947791
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-01-23 14:32:18 +0000

    no idea

--- chatroom test/index.php
+++ chatroom test/index.php
@@ -11,6 +11,31 @@ if(!isset($_SESSION['name'])){
 }
 else{
 ?>
+ <?
+session_start();
+ 
+function loginForm(){
+    echo'
+    <div id="loginform">
+    <form action="index.php" method="post">
+        <p>Please enter your name to continue:</p>
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" />
+        <input type="submit" name="enter" id="enter" value="Enter" />
+    </form>
+    </div>
+    ';
+}
+ 
+if(isset($_POST['enter'])){
+    if($_POST['name'] != ""){
+        $_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
+    }
+    else{
+        echo '<span class="error">Please type in a name</span>';
+    }
+}
+?>
 <div id="wrapper">
     <div id="menu">
         <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
