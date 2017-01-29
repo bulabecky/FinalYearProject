c385b39b8dd248475019b3e751a3d83cfe78307a
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-01-29 13:30:38 +0000

    pwetty pwease

---
+++ LOGIN/dbconnect.php
@@ -0,0 +1,21 @@
+<?php
+
+	// this will avoid mysql_connect() deprecation error.
+	error_reporting( ~E_DEPRECATED & ~E_NOTICE );
+	// but I strongly suggest you to use PDO or MySQLi.
+	
+	define('DBHOST', '178.62.80.200');
+	define('DBUSER', 'root');
+	define('DBPASS', '');
+	define('DBNAME', 'FYP');
+	
+	$conn = mysql_connect(DBHOST,DBUSER,DBPASS);
+	$dbcon = mysql_select_db(DBNAME);
+	
+	if ( !$conn ) {
+		die("Connection failed : " . mysql_error());
+	}
+	
+	if ( !$dbcon ) {
+		die("Database Connection failed : " . mysql_error());
+	}
