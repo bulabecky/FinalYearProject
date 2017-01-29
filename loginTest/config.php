462f507e593d7eea34949ce5aca2d3d6efcce111
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-01-29 17:47:47 +0000

    im dying here

--- loginTest/config.php
+++ loginTest/config.php
@@ -1,7 +1,7 @@
-<? php
-	define('DB_SERVER', 'localhost:178.62.80.200');
-	define('DB_USERNAME', 'root');
-   	define('DB_PASSWORD', 'Beckyboo4');
-   	define('DB_DATABASE', 'users_FYP');
-   	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+<?php
+   define('DB_SERVER', '178.62.80.200');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', 'FYP');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
--- loginTest/login.php
+++ loginTest/login.php
@@ -1,139 +1,47 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Coding Cage - Login & Registration System</title>
-<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
-<link rel="stylesheet" href="style.css" type="text/css" />
+<meta charset="utf-8">
+<title>Login</title>
+<link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
 <?php
- ob_start();
- session_start();
- require_once 'dbconnect.php';
- 
- // it will never let you open index(login) page if session is set
- if ( isset($_SESSION['user'])!="" ) {
-  header("Location: home.php");
-  exit;
- }
- 
- $error = false;
- 
- if( isset($_POST['btn-login']) ) { 
-  
-  // prevent sql injections/ clear user invalid inputs
-  $email = trim($_POST['email']);
-  $email = strip_tags($email);
-  $email = htmlspecialchars($email);
-  
-  $pass = trim($_POST['pass']);
-  $pass = strip_tags($pass);
-  $pass = htmlspecialchars($pass);
-  // prevent sql injections / clear user invalid inputs
-  
-  if(empty($email)){
-   $error = true;
-   $emailError = "Please enter your email address.";
-  } else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
-   $error = true;
-   $emailError = "Please enter valid email address.";
-  }
-  
-  if(empty($pass)){
-   $error = true;
-   $passError = "Please enter your password.";
-  }
-  
-  // if there's no error, continue to login
-  if (!$error) {
-   
-   $password = hash('sha256', $pass); // password hashing using SHA256
-  
-   $res=mysql_query("SELECT userId, userName, userPass FROM users WHERE userEmail='$email'");
-   $row=mysql_fetch_array($res);
-   $count = mysql_num_rows($res); // if uname/pass correct it returns must be 1 row
-   
-   if( $count == 1 && $row['userPass']==$password ) {
-    $_SESSION['user'] = $row['userId'];
-    header("Location: home.php");
-   } else {
-    $errMSG = "Incorrect Credentials, Try again...";
-   }
-    
-  }
-  
- }
+require('db.php');
+session_start();
+// If form submitted, insert values into the database.
+if (isset($_POST['username'])){
+        // removes backslashes
+	$username = stripslashes($_REQUEST['username']);
+        //escapes special characters in a string
+	$username = mysqli_real_escape_string($con,$username);
+	$password = stripslashes($_REQUEST['password']);
+	$password = mysqli_real_escape_string($con,$password);
+	//Checking is user existing in the database or not
+        $query = "SELECT * FROM `users` WHERE username='$username'
+and password='".md5($password)."'";
+	$result = mysqli_query($con,$query) or die(mysql_error());
+	$rows = mysqli_num_rows($result);
+        if($rows==1){
+	    $_SESSION['username'] = $username;
+            // Redirect user to index.php
+	    header("Location: index.php");
+         }else{
+	echo "<div class='form'>
+<h3>Username/password is incorrect.</h3>
+<br/>Click here to <a href='login.php'>Login</a></div>";
+	}
+    }else{
 ?>
-
-<div class="container">
-
- <div id="login-form">
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
-    
-     <div class="col-md-12">
-        
-         <div class="form-group">
-             <h2 class="">Sign In.</h2>
-            </div>
-        
-         <div class="form-group">
-             <hr />
-            </div>
-            
-            <?php
-   if ( isset($errMSG) ) {
-    
-    ?>
-    <div class="form-group">
-             <div class="alert alert-danger">
-    <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
-                </div>
-             </div>
-                <?php
-   }
-   ?>
-            
-            <div class="form-group">
-             <div class="input-group">
-                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
-             <input type="email" name="email" class="form-control" placeholder="Your Email" value="<?php echo $email; ?>" maxlength="40" />
-                </div>
-                <span class="text-danger"><?php echo $emailError; ?></span>
-            </div>
-            
-            <div class="form-group">
-             <div class="input-group">
-                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-             <input type="password" name="pass" class="form-control" placeholder="Your Password" maxlength="15" />
-                </div>
-                <span class="text-danger"><?php echo $passError; ?></span>
-            </div>
-            
-            <div class="form-group">
-             <hr />
-            </div>
-            
-            <div class="form-group">
-             <button type="submit" class="btn btn-block btn-primary" name="btn-login">Sign In</button>
-            </div>
-            
-            <div class="form-group">
-             <hr />
-            </div>
-            
-            <div class="form-group">
-             <a href="register.php">Sign Up Here...</a>
-            </div>
-        
-        </div>
-   
-    </form>
-    </div> 
-
+<div class="form">
+<h1>Log In</h1>
+<form action="" method="post" name="login">
+<input type="text" name="username" placeholder="Username" required />
+<input type="password" name="password" placeholder="Password" required />
+<input name="submit" type="submit" value="Login" />
+</form>
+<p>Not registered yet? <a href='registration.php'>Register Here</a></p>
 </div>
-
+<?php } ?>
 </body>
 </html>
-<?php ob_end_flush(); ?>
