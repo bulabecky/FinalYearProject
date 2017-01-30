553464c279ee6abc5b86615bb4066824c743258c
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-01-30 12:11:31 +0000

    please work

--- loginTest/logout.php
+++ loginTest/logout.php
@@ -1,9 +1,14 @@
 <?php
 session_start();
-// Destroying All Sessions
-if(session_destroy())
-{
-// Redirecting To Home Page
-header("Location: login.php");
+
+if (!isset($_SESSION['userSession'])) {
+ header("Location: index.php");
+} else if (isset($_SESSION['userSession'])!="") {
+ header("Location: home.php");
 }
-?>
+
+if (isset($_GET['logout'])) {
+ session_destroy();
+ unset($_SESSION['userSession']);
+ header("Location: index.php");
+}
