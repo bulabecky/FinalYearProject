67e1820a5c8813c2a75d77683890eac68919dc05
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-01-30 12:38:18 +0000

    loosing it

--- loginTest/index.php
+++ loginTest/index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php
 session_start();
 require_once 'dbconnect.php';
@@ -31,7 +32,6 @@ if (isset($_POST['btn-login'])) {
 	$DBcon->close();
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
