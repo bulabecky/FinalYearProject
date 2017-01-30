b07eb421e5b440ee49bcb510eb29209d00f9952e
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-01-30 12:22:09 +0000

    please work

--- loginTest/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', '178.62.80.200');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'FYP');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
