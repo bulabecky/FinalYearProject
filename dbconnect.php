c8e6a18a017526b25cf4d062ece06ebd570b54d7
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-01 17:25:44 +0000

    changed sql commands (apparently wrong)

--- dbconnect.php
+++ dbconnect.php
@@ -9,14 +9,14 @@
  define('DBPASS', '');
  define('DBNAME', 'register');
  
- $conn = mysql_connect(DBHOST,DBUSER,DBPASS);
- $dbcon = mysql_select_db(DBNAME);
+ $conn = mysqli_connect(DBHOST,DBUSER,DBPASS);
+ $dbcon = mysqli_select_db(DBNAME);
  
  if ( !$conn ) {
-  die("Connection failed : " . mysql_error());
+  die("Connection failed : " . mysqli_error());
  }
  
  if ( !$dbcon ) {
-  die("Database Connection failed : " . mysql_error());
+  die("Database Connection failed : " . mysqli_error());
  }
  ?>
