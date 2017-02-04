0ca37abd82dc0f2a78f59894844d5ec8645363e0
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-04 18:12:41 +0000

    Changes to try and connect to database

--- mapDBconnect.php
+++ mapDBconnect.php
@@ -1,22 +1,5 @@
 <?php
-
- // this will avoid mysql_connect() deprecation error.
- error_reporting( ~E_DEPRECATED & ~E_NOTICE );
- // but I strongly suggest you to use PDO or MySQLi.
- 
- define('DBHOST', 'localhost');
- define('DBUSER', 'root');
- define('DBPASS', 'Beckyboo4');
- define('DBNAME', 'FYP');
- 
- $conn = mysqli_connect(DBHOST,DBUSER,DBPASS);
- $dbcon = mysqli_select_db(DBNAME);
- 
- if ( !$conn ) {
-  die("Connection failed : " . mysqli_error());
- }
- 
- if ( !$dbcon ) {
-  die("Database Connection failed : " . mysqli_error());
- }
- ?>
+$username="root";
+$password="Beckyboo4";
+$database="FYP";
+?>
