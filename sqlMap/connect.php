85267dbcc332c495e042ea2b8f649be49761c585
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-07 20:12:27 +0000

    test

--- sqlMap/connect.php
+++ sqlMap/connect.php
@@ -1,7 +1,10 @@
-<?php 
-  $con = new mysqli("localhost", "root", "Beckyboo4", "FYP");
- 
-  if($con->connect_errno > 0){
-    die('Unable to connect to database [' . $con->connect_error . ']');
+<?php
+// Enter your Host, username, password, database below.
+// I left password empty because i do not set password on localhost.
+$con = mysqli_connect("localhost","root","Beckyboo4","FYP");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 ?>
