6427ab0eafc87f56b0c4227c36f6aabf54681e03
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-07 19:15:28 +0000

    seriously

--- maptest.php
+++ maptest.php
@@ -1,47 +1,53 @@
 <?php
-require("mapDBconnect.php");
+require("pmapDBcinnect.php");
 
-// Start XML file, create parent node
-$doc = domxml_new_doc("1.0");
-$node = $doc->create_element("markers");
-$parnode = $doc->append_child($node);
+function parseToXML($htmlStr)
+{
+$xmlStr=str_replace('<','&lt;',$htmlStr);
+$xmlStr=str_replace('>','&gt;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace("&",'&amp;',$xmlStr);
+return $xmlStr;
+}
 
 // Opens a connection to a MySQL server
-$connection = mysqli_connect ('localhost', $username, $password);
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+$connection=mysql_connect ('localhost', $username, $password);
+if (!$connection) {
+  die('Not connected : ' . mysql_error());
 }
 
 // Set the active MySQL database
-$db_selected = mysqli_select_db($database, $connection);
+$db_selected = mysql_select_db($database, $connection);
 if (!$db_selected) {
-  die ('Can\'t use db : ' . mysqli_error());
+  die ('Can\'t use db : ' . mysql_error());
 }
 
 // Select all the rows in the markers table
 $query = "SELECT * FROM markers WHERE 1";
-$result = mysqli_query($query);
+$result = mysql_query($query);
 if (!$result) {
-  die('Invalid query: ' . mysqli_error());
+  die('Invalid query: ' . mysql_error());
 }
 
 header("Content-type: text/xml");
 
-// Iterate through the rows, adding XML nodes for each
-while ($row = mysqli_fetch_assoc($result)){
-  // Add to XML document node
-  $node = $doc->create_element("marker");
-  $newnode = $parnode->append_child($node);
+// Start XML file, echo parent node
+echo '<markers>';
 
-  $newnode->set_attribute("name", $row['name']);
-  $newnode->set_attribute("address", $row['address']);
-  $newnode->set_attribute("lat", $row['lat']);
-  $newnode->set_attribute("lng", $row['lng']);
-  $newnode->set_attribute("type", $row['type']);
+// Iterate through the rows, printing XML nodes for each
+while ($row = @mysql_fetch_assoc($result)){
+  // Add to XML document node
+  echo '<marker ';
+  echo 'name="' . parseToXML($row['name']) . '" ';
+  echo 'address="' . parseToXML($row['address']) . '" ';
+  echo 'lat="' . $row['lat'] . '" ';
+  echo 'lng="' . $row['lng'] . '" ';
+  echo 'type="' . $row['type'] . '" ';
+  echo '/>';
 }
 
-$xmlfile = $doc->dump_mem();
-echo $xmlfile;
+// End XML file
+echo '</markers>';
 
 ?>
