f6dd65d663b5c3747af75df58cd99817de533381
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-07 18:48:08 +0000

    please

--- maptest.php
+++ maptest.php
@@ -1,100 +1,46 @@
-<!DOCTYPE html >
-  <head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-    <title>Using MySQL and PHP with Google Maps</title>
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
-  </head>
+<?php
+require("mapDBconnect.php");
 
-  <body>
-    <div id="map"></div>
+// Start XML file, create parent node
+$doc = domxml_new_doc("1.0");
+$node = $doc->create_element("markers");
+$parnode = $doc->append_child($node);
 
-    <script>
-      var customLabel = {
-        restaurant: {
-          label: 'R'
-        },
-        bar: {
-          label: 'B'
-        }
-      };
+// Opens a connection to a MySQL server
+$connection=mysqli_connect ('localhost', $username, $password);
+if (!$connection) {
+  die('Not connected : ' . mysqli_error());
+}
 
-        function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: new google.maps.LatLng(-33.863276, 151.207977),
-          zoom: 12
-        });
-        var infoWindow = new google.maps.InfoWindow;
+// Set the active MySQL database
+$db_selected = mysqli_select_db($database, $connection);
+if (!$db_selected) {
+  die ('Can\'t use db : ' . mysqli_error());
+}
 
-          // Change this depending on the name of your PHP or XML file
-          downloadUrl('https://storage.googleapis.com/mapsdevsite/json/mapmarkers2.xml', function(data) {
-            var xml = data.responseXML;
-            var markers = xml.documentElement.getElementsByTagName('marker');
-            Array.prototype.forEach.call(markers, function(markerElem) {
-              var name = markerElem.getAttribute('name');
-              var address = markerElem.getAttribute('address');
-              var type = markerElem.getAttribute('type');
-              var point = new google.maps.LatLng(
-                  parseFloat(markerElem.getAttribute('lat')),
-                  parseFloat(markerElem.getAttribute('lng')));
+// Select all the rows in the markers table
+$query = "SELECT * FROM markers WHERE 1";
+$result = mysqli_query($query);
+if (!$result) {
+  die('Invalid query: ' . mysqli_error());
+}
 
-              var infowincontent = document.createElement('div');
-              var strong = document.createElement('strong');
-              strong.textContent = name
-              infowincontent.appendChild(strong);
-              infowincontent.appendChild(document.createElement('br'));
+header("Content-type: text/xml");
 
-              var text = document.createElement('text');
-              text.textContent = address
-              infowincontent.appendChild(text);
-              var icon = customLabel[type] || {};
-              var marker = new google.maps.Marker({
-                map: map,
-                position: point,
-                label: icon.label
-              });
-              marker.addListener('click', function() {
-                infoWindow.setContent(infowincontent);
-                infoWindow.open(map, marker);
-              });
-            });
-          });
-        }
+// Iterate through the rows, adding XML nodes for each
+while ($row = mysqli_fetch_assoc($result)){
+  // Add to XML document node
+  $node = $doc->create_element("marker");
+  $newnode = $parnode->append_child($node);
 
+  $newnode->set_attribute("name", $row['name']);
+  $newnode->set_attribute("address", $row['address']);
+  $newnode->set_attribute("lat", $row['lat']);
+  $newnode->set_attribute("lng", $row['lng']);
+  $newnode->set_attribute("type", $row['type']);
+}
 
+$xmlfile = $doc->dump_mem();
+echo $xmlfile;
 
-      function downloadUrl(url, callback) {
-        var request = window.ActiveXObject ?
-            new ActiveXObject('Microsoft.XMLHTTP') :
-            new XMLHttpRequest;
-
-        request.onreadystatechange = function() {
-          if (request.readyState == 4) {
-            request.onreadystatechange = doNothing;
-            callback(request, request.status);
-          }
-        };
-
-        request.open('GET', url, true);
-        request.send(null);
-      }
-
-      function doNothing() {}
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAJXSQg6uRk9OD-fGID7NQ52sXpufXz268&callback=initMap">
-    </script>
-  </body>
-</html>
+?>
