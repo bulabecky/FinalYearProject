5c87798a3d44463d6d4ebbbf6c8a70e7723133c4
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-08 20:26:01 +0000

    new index please work

--- LeafletMap/testIndex.js
+++ LeafletMap/testIndex.js
@@ -1,6 +1,6 @@
-var mymap = L.map('mapid').setView([53.350140, -6.266155], 6)
+var mymap = L.map('mapid').setView([53.350140, -6.266155], 13);
     
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 17,
       attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
     }).addTo(mymap);
