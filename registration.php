28d4c23bfd51b19898b5ea2fbe69aa1adf5d9022
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-08 09:52:22 +0000

    test

--- registration.php
+++ registration.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title>Registration</title>
-<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="style1.css" />
 </head>
 <body>
     <!DOCTYPE html>
