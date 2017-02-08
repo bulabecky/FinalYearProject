158e7d6b31c376e932bb3473b9c35af77b27ffd2
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-08 16:10:48 +0000

    New map added via open street map
    Trying to get rid of any redundant code and implement new map

---
+++ LeafletMap/testIndex.js
@@ -0,0 +1,18 @@
+var mymap = L.map('mapid').setView([53.350140, -6.266155], 13);
+    
+    L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      maxZoom: 17,
+      attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    }).addTo(mymap);
+
+    var popup = L.popup(); 
+
+    function onMapClick(e) {
+      popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+        var LatLong = e.latlng.toString()
+    }
+
+    mymap.on('click', onMapClick);    
--- logout.php
+++ logout.php
@@ -1,3 +1,19 @@
+<<<<<<< HEAD:loginTest/logout.php
+<?php
+session_start();
+
+if (!isset($_SESSION['userSession'])) {
+ header("Location: index.php");
+} else if (isset($_SESSION['userSession'])!="") {
+ header("Location: home.php");
+}
+
+if (isset($_GET['logout'])) {
+ session_destroy();
+ unset($_SESSION['userSession']);
+ header("Location: index.php");
+}
+=======
 <?php
 session_start();
 // Destroying All Sessions
@@ -7,3 +23,4 @@ if(session_destroy())
 header("Location: login.php");
 }
 ?>
+>>>>>>> 28d4c23bfd51b19898b5ea2fbe69aa1adf5d9022:logout.php
