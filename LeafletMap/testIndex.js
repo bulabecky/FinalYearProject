83c33522f5afcb078b3091e666e69f4991f6fd7f
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-12 18:15:23 +0000

    please work

--- LeafletMap/testIndex.js
+++ LeafletMap/testIndex.js
@@ -15,4 +15,15 @@ var mymap = L.map('mapid').setView([53.350140, -6.266155], 13);
         var LatLong = e.latlng.toString()
     }
 
+    var band = L.icon({
+      iconUrl: 'showband.png'
+
+      iconSize:     [38, 95], // size of the icon
+      //shadowSize:   [50, 64], // size of the shadow
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      //shadowAnchor: [4, 62],  // the same for the shadow
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
     mymap.on('click', onMapClick);    
+    L.marker([53.35, -6.25], {icon: band}).addTo(map).bindPopup("Can you see me?");
