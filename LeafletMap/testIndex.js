d55ae096f23d4df229fb49c0c4ff581b3eb9a631
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-13 16:41:54 +0000

    cork boy

--- LeafletMap/testIndex.js
+++ LeafletMap/testIndex.js
@@ -5,7 +5,7 @@ var mymap = L.map('mapid').setView([53.350140, -6.266155], 13);
       attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
     }).addTo(mymap);
 
-    var marker = L.marker([53.350140, -6.266155]).addTo(mymap);
+    
 
     var popup = L.popup(); 
 
