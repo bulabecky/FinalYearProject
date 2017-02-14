1c7685f98c7aa8db2b4f507548f1415ef0b005b7
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-14 10:22:40 +0000

    adding kml to logged in map

--- login/index.php
+++ login/index.php
@@ -13,13 +13,13 @@ include("auth.php");
     <title>Cosán Ceol</title>
 
        <!-- Bootstrap Core CSS -->
-    <link href="css1/bootstrap1.min.css" rel="stylesheet">
+    <link href="../css1/bootstrap1.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="css1/stylish-portfolio.css" rel="stylesheet">
+    <link href="../css1/stylish-portfolio.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="../font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
     
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.2/dist/leaflet.css" />
@@ -89,7 +89,7 @@ include("auth.php");
             </div>
         </section>
 
-    <!-- Map -->
+    
    <!-- Map -->
     <section id="map" class="map">
         <div id="mapid"  style="width: 100%; height: 700px;"></div>
