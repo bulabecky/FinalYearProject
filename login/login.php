d288f3077fb930ec7d83b5866fc0288ca1d7ebb1
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-14 10:35:23 +0000

    getting rid of unneeded link

--- login/login.php
+++ login/login.php
@@ -88,7 +88,7 @@ and password='".md5($password)."'";
 <input name="submit" type="submit" value="Login" />
 </form>
 <p style="color:white;">Not registered yet? <a href='registration.php'>Register Here</a></p>
-<p style="color:white;">Not registered yet? <a href='index.html'>Home</a></p>
+
 
 </div>
 <?php } ?>
