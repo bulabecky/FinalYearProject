8dd5c9565a0cef6a4f160068b84682a0f5abac7d
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-14 17:28:14 +0000

    test

--- LeafletMap/testIndex.js
+++ LeafletMap/testIndex.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-var mymap = L.map('mapid').setView([53.350140, -6.266155], 13);
-    
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 17,
-      attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-    }).addTo(mymap);
-
-    var popup = L.popup(); 
-
-    function onMapClick(e) {
-      popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
-        var LatLong = e.latlng.toString()
-    }
-
-    mymap.on('click', onMapClick);    
-=======
-var mymap = L.map('mapid').setView([53.350140, -6.266155], 13);
-    
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 17,
-      attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-    }).addTo(mymap);
-
-    var popup = L.popup(); 
-
-    function onMapClick(e) {
-      popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
-        var LatLong = e.latlng.toString()
-    }
-  mymap.on('click', onMapClick);    
-
-var marker = L.marker([53.35096,-6.25465]).addTo(mymap);
->>>>>>> 98959eaad8f442f3ce13cf71f91384bc0cde9b66
-=======
 var mymap = L.map('mapid').setView([53.350140, -6.266155], 13);
     
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -66,4 +24,3 @@ var mymap = L.map('mapid').setView([53.350140, -6.266155], 13);
   });
 
   mymap.addLayer(kmlLayer); 
->>>>>>> d288f3077fb930ec7d83b5866fc0288ca1d7ebb1
