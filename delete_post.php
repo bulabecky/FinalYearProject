4faae3781b8c06aa4b339944d8d19d1f4d8fde5e
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-20 17:46:46 +0000

    made a mistake

--- delete_post.php
+++ delete_post.php
@@ -3,7 +3,7 @@ include('dbcon.php');
 $id=$_GET['id'];
 
 mysqli_query("delete from post where post_id='$id'")or die(mysqli_error());
-header('location:index.php');
+header('location:index1.php');
 
 
 
