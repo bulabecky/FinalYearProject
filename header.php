10142c3ed27a6268d28e69e48ea86424aa9a2a8b
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-20 17:59:14 +0000

    please please work

--- header.php
+++ header.php
@@ -4,12 +4,12 @@
 
         <title>POST</title>
    
-        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="screen">
-        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" media="screen">
+        <link href="css-test/bootstrap.css" rel="stylesheet" type="text/css" media="screen">
+        <link href="css-test/font-awesome.min.css" rel="stylesheet" type="text/css" media="screen">
        
        
     </head>
-<script src="js/jquery.js" type="text/javascript"></script>
-<script src="js/bootstrap.js" type="text/javascript"></script>
+<script src="js1/jquery.js" type="text/javascript"></script>
+<script src="js1/bootstrap.js" type="text/javascript"></script>
 
 <?php include('dbcon.php'); ?>
