d2e9a3e8f29142d90ecceee8a7645662edba5151
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-21 13:42:46 +0000

    Ajax Issue

--- comments.php
+++ comments.php
@@ -2,7 +2,7 @@
 <head>
 <link rel="stylesheet" type="text/css" href="comment_style.css">
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.js" />
-<script type="text/javascript">
+<script>
 function post()
 {
   console.log("POSTING BITCH");
