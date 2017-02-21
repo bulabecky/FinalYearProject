a954aaead9b306cbbc2a670e17680e6e8a22b3ab
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-21 19:29:08 +0000

    Ajax Issue

--- post_comment.php
+++ post_comment.php
@@ -10,13 +10,13 @@ $con = mysqli_connect("localhost","root","Beckyboo4","register");
 
     if(isset($_POST['user_comm']) && isset($_POST['user_name'])){
       date_default_timezone_set('Europe/Dublin');
-      $date = date('Y-m-d H:i:s', time());
+      $date = date("Y-m-d H:i:s");
 
 
       $name = $_POST['user_name'];
       $comment = $_POST['user_comm'];
 
-      $insert = mysqli_query($con,"INSERT INTO comments(name, comment, post_time) VALUES ('$name','$comment', '2017-02-21 19:32:32')");
+      $insert = mysqli_query($con,"INSERT INTO comments(name, comment, post_time) VALUES ('$name','$comment', '$date')");
 
           
           ?>
