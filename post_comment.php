3a6df1cadc7916fe85ad05a2b7cb2aa9c8df6627
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-21 18:16:05 +0000

    Ajax Issue

--- post_comment.php
+++ post_comment.php
@@ -1,5 +1,5 @@
 <?php
-
+echo("<script>console.log('PHP');</script>");
 $con = mysqli_connect("localhost","root","Beckyboo4","register");
     // Check connection
 if (mysqli_connect_errno())
@@ -7,7 +7,7 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-  echo("<script>console.log('PHP');</script>");
+  
 
 
 
