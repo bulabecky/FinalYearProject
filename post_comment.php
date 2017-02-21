ead7580104dac8bddb1968dab4fda131943c90c9
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-21 18:13:17 +0000

    Ajax Issue

--- post_comment.php
+++ post_comment.php
@@ -7,30 +7,9 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-echo("<script>console.log('HELLO MOTTO');</script>");
+echo('<script>console.log("HELLO MOTTO");</script>');
 
-if(isset($_POST['user_comm']) && isset($_POST['user_name']))
-{
-  $comment=$_POST['user_comm'];
-  $name=$_POST['user_name'];
-  $insert=mysqli_query("insert into comments values('2','Becky','Right durr','2017-02-21 16:15')");
-  
-  $id=mysqli_insert_id($insert);
 
-  $select=mysqli_query("select name,comment,post_time from comments where name='$name' and comment='$comment' and id='$id'");
-  
-  if($row=mysqli_fetch_array($select))
-  {
-	  $name=$row['name'];
-	  $comment=$row['comment'];
-      $time=$row['post_time'];
-  ?>
-      <div class="comment_div"> 
-	    <p class="name">Posted By:<?php echo $name;?></p>
-        <p class="comment"><?php echo $comment;?></p>	
-	    <p class="time"><?php echo $time;?></p>
-	  </div>
-  <?php
-  }
-exit;
-}?>
+
+
+?>
