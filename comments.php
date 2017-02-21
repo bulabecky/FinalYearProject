0bdf872397085ecb2bbf30e869ca190d0454a003
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-21 17:19:37 +0000

    Ajax Issue

--- comments.php
+++ comments.php
@@ -31,7 +31,7 @@
     echo("<script>console.log('FOR THE LOVE OF GAWD WORK');</script>");
 
 
-    $result = mysql_query('SELECT * FROM comments');
+    $result = mysqli_query('SELECT * FROM comments;');
     if (!$result) {
         die('Could not query:' . mysqli_error());
     }
