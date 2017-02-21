4aec363873d92bcca570db0373f5c4bb80f8eba2
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-21 13:25:44 +0000

    trying something

--- comments.php
+++ comments.php
@@ -1,36 +1,7 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="comment_style.css">
-<script type="text/javascript" src="js/jquery.js">
-<script type="text/javascript">
-function post()
-{
-  var comment = document.getElementById("comment").value;
-  var name = document.getElementById("username").value;
-  if(comment && name)
-  {
-    $.ajax
-    ({
-      type: 'post',
-      url: 'post_comment.php',
-      data: 
-      {
-         user_comm:comment,
-       user_name:name
-      },
-      success: function (response) 
-      {
-      document.getElementById("all_comments").innerHTML=response+document.getElementById("all_comments").innerHTML;
-      document.getElementById("comment").value="";
-        document.getElementById("username").value="";
-  
-      }
-    });
-  }
-  
-  return false;
-}
-</script>
+
 
 </head>
 
@@ -69,7 +40,36 @@ function post()
       <p class="comment"><?php echo $comment;?></p> 
     <p class="time"><?php echo $time;?></p>
   </div>
+  <script type="text/javascript" src="js/jquery.js">
+<script type="text/javascript">
+function post()
+{
+  var comment = document.getElementById("comment").value;
+  var name = document.getElementById("username").value;
+  if(comment && name)
+  {
+    $.ajax
+    ({
+      type: 'post',
+      url: 'post_comment.php',
+      data: 
+      {
+         user_comm:comment,
+       user_name:name
+      },
+      success: function (response) 
+      {
+      document.getElementById("all_comments").innerHTML=response+document.getElementById("all_comments").innerHTML;
+      document.getElementById("comment").value="";
+        document.getElementById("username").value="";
   
+      }
+    });
+  }
+  
+  return false;
+}
+</script>
     <?php
     }
     ?>
