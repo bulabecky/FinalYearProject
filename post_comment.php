9110d752391303f26232dda91a0e19af7f2cadda
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-21 18:21:16 +0000

    Ajax Issue

--- post_comment.php
+++ post_comment.php
@@ -8,9 +8,16 @@ $con = mysqli_connect("localhost","root","Beckyboo4","register");
       }
 
 
-    $result = mysqli_query($con,'SELECT name, comment, post_time FROM commments');
-    if (!$result) {
-        die('Could not query:' . mysqli_error());
+    if(isset($_POST['user_comm']) && isset($_POST['user_name'])){
+
+      $result = mysqli_query($con,'SELECT name, comment, post_time FROM commments');
+        if (!$result) {
+            die('Could not query:' . mysqli_error());
+        }
+
+
+
+
     }
 
 
