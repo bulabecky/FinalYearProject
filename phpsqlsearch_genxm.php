e84bceba87349a27b3e8f94aff5b90ed44cfab89
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-22 16:35:27 +0000

    testing

--- phpsqlsearch_genxm.php
+++ phpsqlsearch_genxm.php
@@ -19,7 +19,7 @@ if (!$db_selected) {
 }
 
 // Select all the rows in the markers table
-$query = "SELECT * FROM markers WHERE 1";
+$query = ($connection,"SELECT * FROM markers WHERE 1");
 $result = mysqli_query($query);
 if (!$result) {
   die('Invalid query: ' . mysqli_error());
