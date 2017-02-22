2d03e90604d62bab77ea57867777d5b3f05fa8ab
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-22 13:26:41 +0000

    trying something quickly with google maps

--- phpsqlsearch_genxm.php
+++ phpsqlsearch_genxm.php
@@ -12,10 +12,12 @@ return $xmlStr;
 }
 
 // Opens a connection to a mysqli server
-$connection=mysqlii_connect ('localhost', $username, $password);
-if (!$connection) {
-  die('Not connected : ' . mysqli_error());
-}
+$con = mysqli_connect("localhost","root","Beckybo4","FYP");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
 
 // Set the active mysqli database
 $db_selected = mysqli_select_db($database, $connection);
@@ -24,7 +26,7 @@ if (!$db_selected) {
 }
 
 // Select all the rows in the markers table
-$query = "SELECT * FROM markers WHERE 1";
+$query = ($con,"SELECT * FROM markers WHERE 1");
 $result = mysqli_query($query);
 if (!$result) {
   die('Invalid query: ' . mysqli_error());
