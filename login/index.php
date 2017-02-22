5585f3ed91cea0b71df7ee38b5b2a132053245b6
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-22 10:37:03 +0000

    styling

--- login/index.php
+++ login/index.php
@@ -93,6 +93,47 @@ include("auth.php");
         <div id="mapid"  style="width: 100%; height: 700px;"></div>
         <script src="../../LeafletMap/KML.js"></script>
         <script src="../../LeafletMap/testIndex.js"></script>
+        <form method="post" action="" onsubmit="return post();">
+  <textarea id="comment" placeholder="Write Your Comment Here....."></textarea>
+  <br>
+  <input type="text" id="username" placeholder="Your Name">
+  <br>
+  <input type="submit" value="Post Comment">
+  </form>
+
+  <div id="all_comments">
+  <?php
+    $con = mysqli_connect("localhost","root","Beckyboo4","register");
+    // Check connection
+    if (mysqli_connect_errno())
+      {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+      }
+
+
+    $result = mysqli_query($con,'SELECT name, comment, post_time FROM comments order by post_time desc');
+    if (!$result) {
+        die('Could not query:' . mysqli_error());
+    }
+
+   while ($row = mysqli_fetch_row($result)) {
+      $name = $row[0];
+      $comment = $row[1];
+      $datetime = $row[2];
+      
+      ?>
+
+      <div class="comment_div"> 
+        <p class="name">Posted By: <?php echo $name;?></p>
+          <p class="comment"><?php echo $comment;?></p> 
+        <p class="time"><?php echo $datetime;?></p>
+      </div>
+    <?php
+    }
+
+    mysqli_close($con) or die(mysqli_error());
+    ?>
+  </div>
     </section>
 
     <!-- Footer -->
