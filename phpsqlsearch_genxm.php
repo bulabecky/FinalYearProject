0eb35b2bd2c93e9b516b1d5e28246a7ce1a7fc70
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-22 17:07:15 +0000

    I broke somehting

--- phpsqlsearch_genxm.php
+++ phpsqlsearch_genxm.php
@@ -1,14 +1,15 @@
 <?php
+
+
 // Start XML file, create parent node
-echo("<?xml version="1.0" encoding="UTF-8"?>");
-echo ("<script>console.log('opening of page');</script>");
+
 $dom = new DOMDocument("1.0");
 $node = $dom->createElement("markers");
 $parnode = $dom->appendChild($node);
 
 // Opens a connection to a MySQL server
 
-$con = mysqli_connect("localhost","root","Beckyboo4","map");
+   $con = mysqli_connect("localhost","root","Beckyboo4","map");
     // Check connection
     if (mysqli_connect_errno())
       {
@@ -16,16 +17,16 @@ $con = mysqli_connect("localhost","root","Beckyboo4","map");
       }
 
 
-    $result = mysqli_query($con,'SELECT * FROM `markers` WHERE 1');
+    $result = mysqli_query($con,'SELECT * FROM markers where 1');
     if (!$result) {
         die('Could not query:' . mysqli_error());
     }
 
-
+header("Content-type: text/xml");
 
 // Iterate through the rows, adding XML nodes for each
 
-while ($row = @mysqli_fetch_assoc($result)){
+while ($row = @mysql_fetch_assoc($result)){
   // Add to XML document node
   $node = $dom->createElement("marker");
   $newnode = $parnode->appendChild($node);
