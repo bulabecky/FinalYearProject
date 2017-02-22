23804c751a5a654eb01ad4e841c3fd8be397c72c
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-22 13:02:11 +0000

    trying something quickly with google maps

--- phpsqlsearch_genxm.php
+++ phpsqlsearch_genxm.php
@@ -7,7 +7,7 @@ $xmlStr=str_replace('<','&lt;',$htmlStr);
 $xmlStr=str_replace('>','&gt;',$xmlStr); 
 $xmlStr=str_replace('"','&quot;',$xmlStr); 
 $xmlStr=str_replace("'",'&#39;',$xmlStr); 
-$xmlStr=str_replace("&",'&amp;',$xmlStr); 
+$xmlStr=str_replace("&",'& amp;',$xmlStr); 
 return $xmlStr; 
 } 
 // Get parameters from URL
