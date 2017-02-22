c8a1a5d6823ecab987a4fddeef78fd1c081e7023
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-22 00:57:17 +0000

    styling

--- login/index.php
+++ login/index.php
@@ -90,7 +90,7 @@ include("auth.php");
 
     <!-- Map -->
     <section id="map" class="map">
-        <div id="mapid"></div>
+        <div id="mapid"  style="width: 100%; height: 700px;"></div>
         <script src="../../LeafletMap/KML.js"></script>
         <script src="../../LeafletMap/testIndex.js"></script>
     </section>
