0e7cbce4a10e0e99030a0c6eb68bdcb9ef641920
Rebecca McGowan <mcgowan.rebecca94@gmail.com>
2017-02-22 14:36:23 +0000

    trying to debug

--- phpsqlsearch_genxm.php
+++ phpsqlsearch_genxm.php
@@ -25,6 +25,7 @@ if (!$result) {
   die('Invalid query: ' . mysqli_error());
 }
 
+echo __LINE__;exit;
 
 // Start XML file, echo parent node
 echo ('<markers>');
@@ -34,8 +35,8 @@ while ($row = mysqli_fetch_assoc($result)){
 
   echo ("<script>console.log('Heuston, we are in!');</script>");
   // Add to XML document node
-  
-  echo '<marker ';echo __LINE__;exit;
+
+  echo '<marker ';
   echo 'name="' . parseToXML($row['name']) . '" ';
   echo 'address="' . parseToXML($row['address']) . '" ';
   echo 'lat="' . $row['lat'] . '" ';
