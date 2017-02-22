f1f8d21c18755562bbe0df6b53419f10ab40a967
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-22 21:45:19 +0000

    adding google map to interface

--- index.php
+++ index.php
@@ -168,8 +168,52 @@
       }
 
       function doNothing() {}
-    </script>
+    </script>  
+      <div id="comment-section" style="float:left;">
+    <h1>Leave a comment!</h1>
 
+  <form method="post" action="" onsubmit="return post();">
+  <textarea id="comment" placeholder="Write Your Comment Here....."></textarea>
+  <br>
+  <input type="text" id="username" placeholder="Your Name">
+  <br>
+  <input type="submit" value="Post Comment">
+  </form>
+
+  <div id="all_comments">
+  <?php
+    $con = mysqli_connect("localhost","root","Beckyboo4","register");
+    // Check connection
+    if (mysqli_connect_errno())
+      {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+      }
+
+
+    $result = mysqli_query($con,'SELECT name, comment, post_time FROM comments order by post_time desc');
+    if (!$result) {
+        die('Could not query:' . mysqli_error());
+    }
+
+   while ($row = mysqli_fetch_row($result)) {
+      $name = $row[0];
+      $comment = $row[1];
+      $datetime = $row[2];
+      
+      ?>
+
+      <div class="comment_div"> 
+        <p class="name">Posted By: <?php echo $name;?></p>
+          <p class="comment"><?php echo $comment;?></p> 
+        <p class="time"><?php echo $datetime;?></p>
+      </div>
+    <?php
+    }
+
+    mysqli_close($con) or die(mysqli_error());
+    ?>
+  </div>
+</div>
      <footer id="bottom">
         <div class="container">
             <div class="row">
@@ -201,7 +245,7 @@
                     <p>Copyright &copy; cosanceol.tk 2017</p>
                 </div>
             </div>
-        </div>
+       
         <a id="to-top" href="#top" class="btn btn-dark btn-lg"><i class="fa fa-chevron-up fa-fw fa-1x"></i></a>
     </footer>
     <script src="js/jquery.js"></script>
