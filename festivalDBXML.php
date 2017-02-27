b1d571475a2783c4f32ec411f7e0838a529da779
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-27 19:19:35 +0000

    trying to get link to work

--- festivalDBXML.php
+++ festivalDBXML.php
@@ -38,7 +38,7 @@ while ($row = @mysqli_fetch_assoc($result)){
   echo 'lat="' . $row['lat'] . '" ';
   echo 'lng="' . $row['lng'] . '" ';
   echo 'type="' . $row['type'] . '" ';
-  echo 'website="' . parseToXML('<a href="' . $row['website'] . '">Link Text</a> "') . '" ';
+  echo 'website="' . parseToXML($row['website']) . '" ';
   echo '/>';
 }
 
