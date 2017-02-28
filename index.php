091bcffb3b861397afe5905b234f7249c77cad23
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-28 20:51:48 +0000

    trying to add filter option

--- index.php
+++ index.php
@@ -200,8 +200,9 @@
       }
 
       function doNothing() {}
-    </script>    
-      <div id="comment-section" style="float:left;">
+    </script> 
+    <div class="col-lg-12 text-center">    
+      <div id="comment-section">
           <h4>Leave a comment</h4>
 
   <form method="post" action="" onsubmit="return post();">
@@ -244,6 +245,7 @@
 
     mysqli_close($con) or die(mysqli_error());
     ?>
+  </div>
   </div>
 
      <footer id="bottom">
