a28362fde21a1d5b21c08990f5f78b08d7d7c529
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-02-28 20:08:08 +0000

    trying to add filter option

--- index.php
+++ index.php
@@ -146,14 +146,14 @@
           downloadUrl('http://cosanceol.tk/mapDBXML.php', function(data) {
             var xml = data.responseXML;
             var markers = xml.documentElement.getElementsByTagName('marker');
-             for (var i = 0; i < markers.length; i++) {
-              var name = markers[i].getAttribute('name');
-              var address = markers[i].getAttribute('address');
-              //var video = markers[i]getAttribute('video');
-              var type = markers[i].getAttribute('type');
+            Array.prototype.forEach.call(markers, function(markerElem) {
+              var name = markerElem.getAttribute('name');
+              var address = markerElem.getAttribute('address');
+              var video = markerElem.getAttribute('video');
+              var type = markerElem.getAttribute('type');
               var point = new google.maps.LatLng(
-                  parseFloat(markers[i].getAttribute('lat')),
-                  parseFloat(markers[i].getAttribute('lng')));
+                  parseFloat(markerElem.getAttribute('lat')),
+                  parseFloat(markerElem.getAttribute('lng')));
 
               var infowincontent = document.createElement('div');
               var strong = document.createElement('strong');
@@ -164,32 +164,23 @@
               var text = document.createElement('text');
               text.textContent = address
               infowincontent.appendChild(text);
-             /* var x = document.createElement("IFRAME");
+              var x = document.createElement("IFRAME");
               x.setAttribute("src", video);
-              infowincontent.appendChild(x);*/
-
+              infowincontent.appendChild(x);
               var marker = new google.maps.Marker({
                 map: map,
                 position: point,
                 icon: icons[type].icon
               });
-              marker.mycategory = type;
-              gmarkers.push(marker);
-              // marker.addListener('click', function() {
-              //   infoWindow.setContent(infowincontent);
-              //   infoWindow.open(map, marker);
-              // });
-          
-               bindInfoWindow(marker, map, infoWindow, html);
-            }
-              });
-            }
-            function bindInfoWindow(marker, map, infoWindow, html) {
-              google.maps.event.addListener(marker, 'click', function() {
-                infoWindow.setContent(html);
+              marker.addListener('click', function() {
+                infoWindow.setContent(infowincontent);
                 infoWindow.open(map, marker);
               });
-            }
+            });
+          });
+        }
+
+
 
       function downloadUrl(url, callback) {
         var request = window.ActiveXObject ?
@@ -208,38 +199,7 @@
       }
 
       function doNothing() {}
-
-       function show(category) {
-             for (var i=0; i<gmarkers.length; i++) {
-               if (gmarkers[i].mycategory == category) {
-                 gmarkers[i].setVisible(true);
-               }
-             }
-           // == check the checkbox ==
-            document.getElementById(category+"box").checked = true;
-            }
-
-         // == hides all markers of a particular category, and ensures the checkbox is cleared ==
-         function hide(category) {
-             for (var i=0; i<gmarkers.length; i++) {
-               if (gmarkers[i].mycategory == category) {
-                 gmarkers[i].setVisible(false);
-               }
-             }
-           // == clear the checkbox ==
-            document.getElementById(category+"box").checked = false;
-               // == close the info window, in case its open on a marker that we just hid
-               infoWindow.close();
-             }
-                     // == a checkbox has been clicked ==
-              function boxclick(box,category) {
-                if (box.checked) {
-                  show(category);
-                } else {
-                  hide(category);
-                }
-      }
-    </script>  
+    </script>    
       <div id="comment-section" style="float:left;">
           <h4>Leave a comment</h4>
 
