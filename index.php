327f34cdbf7579c3c74004b6e9b5910ac1c51623
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-03-01 17:39:08 +0000

    trying to add filter option

--- index.php
+++ index.php
@@ -174,16 +174,18 @@
 
           // Change this depending on the name of your PHP or XML file
           downloadUrl('http://cosanceol.tk/mapDBXML.php', function(data) {
-            var xml = data.responseXML;
-            var markers = xml.documentElement.getElementsByTagName("marker");
+         var xml = data.responseXML;
+         var markers = xml.documentElement.getElementsByTagName("marker");
               for (var i = 0; i < markers.length; i++) {
-              var name = markers[i].getAttribute("name");
-              var address = markers[i].getAttribute("address");
-              var video = markers[i].getAttribute("video");
-              var type = markers[i].getAttribute("type");
-              var point = new google.maps.LatLng(
-                  parseFloat(markers[i].getAttribute("lat")),
-                  parseFloat(markers[i].getAttribute("lng")))
+                var name = markers[i].getAttribute("name");
+                var address = markers[i].getAttribute("address");
+                var video = markers[i].getAttribute("video");
+                var type = markers[i].getAttribute("type");
+                var point = new google.maps.LatLng(
+                    parseFloat(markers[i].getAttribute("lat")),
+                    parseFloat(markers[i].getAttribute("lng")));
+                var html = "<b>" + name + "</b> <br/>" + address +"<br/" + video;
+
 
               var infowincontent = document.createElement('div');
               var strong = document.createElement('strong');
@@ -198,70 +200,74 @@
               var x = document.createElement("IFRAME");
               x.setAttribute("src", video);
               infowincontent.appendChild(x);
-              var marker = new google.maps.Marker({
-                map: map,
-                position: point,
-                icon: icons[type].icon
-              });
+             var xml = data.responseXML;
+  var markers = xml.documentElement.getElementsByTagName("marker");
 
-                 marker.mycategory = type;
-                 gmarkers.push(marker);
+    var marker = new google.maps.Marker({
+      map: map,
+      position: point,
+      icon: icon.icon
+    });
+    marker.mycategory = type;
+    gmarkers.push(marker);
 
-                bindInfoWindow(marker, map, infoWindow);
-                  }
-                 });
-                }
+    bindInfoWindow(marker, map, infoWindow, html);
+}
+  });
+}
+
+    function bindInfoWindow(marker, map, infoWindow, html) {
+      google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.setContent(html);
+        infoWindow.open(map, marker);
+      });
+    }
+
+    function downloadUrl(url, callback) {
+      var request = window.ActiveXObject ?
+          new ActiveXObject('Microsoft.XMLHTTP') :
+          new XMLHttpRequest;
+
+      request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+          request.onreadystatechange = doNothing;
+          callback(request, request.status);
+        }
+      };
+
+      request.open('GET', url, true);
+      request.send(null);
+    }
+
+    function doNothing() {}
 
 
-           function bindInfoWindow(marker, map, infoWindow) {
-               marker.addListener('click', function() {
-                infoWindow.setContent(infowincontent);
-                infoWindow.open(map, marker);
-                });
-            };
-
-
-              function downloadUrl(url, callback) {
-                var request = window.ActiveXObject ?
-                    new ActiveXObject('Microsoft.XMLHTTP') :
-                    new XMLHttpRequest;
-
-                request.onreadystatechange = function() {
-                  if (request.readyState == 4) {
-                    request.onreadystatechange = doNothing;
-                    callback(request, request.status);
-                  }
-                };
-
-                request.open('GET', url, true);
-                request.send(null);
-              }
-
-              function doNothing() {}
-
-       function show(category) {
-         for (var i=0; i<gmarkers.length; i++) {
-           if (gmarkers[i].mycategory == category) {
-             gmarkers[i].setVisible(true);
-           }
-         }
-         // == check the checkbox ==
-         document.getElementById(category+"box").checked = true;
-       }
+// == shows all markers of a particular category, and ensures the checkbox is checked ==
+ function show(category) {
+   for (var i=0; i<gmarkers.length; i++) {
+     if (gmarkers[i].mycategory == category) {
+       gmarkers[i].setVisible(true);
+     }
+   }
+   // == check the checkbox ==
+   document.getElementById(category+"box").checked = true;
+ }
 
  // == hides all markers of a particular category, and ensures the checkbox is cleared ==
-     function hide(category) {
-         for (var i=0; i<gmarkers.length; i++) {
-           if (gmarkers[i].mycategory == category) {
-             gmarkers[i].setVisible(false);
-           }
-         }
-         // == clear the checkbox ==
-         document.getElementById(category+"box").checked = false;
-         // == close the info window, in case its open on a marker that we just hid
+ function hide(category) {
+   for (var i=0; i<gmarkers.length; i++) {
+     if (gmarkers[i].mycategory == category) {
+       gmarkers[i].setVisible(false);
+     }
+   }
+   // == clear the checkbox ==
+   document.getElementById(category+"box").checked = false;
+   // == close the info window, in case its open on a marker that we just hid
+   infoWindow.close();
+ }
 
-       }
-          function boxclick(box,category) {
+ // == a checkbox has been clicked ==
+      function boxclick(box,category) {
         if (box.checked) {
           show(category);
         } else {
@@ -269,7 +275,7 @@
         }
       }
 
-
+    //]]>
 
 
     </script>
