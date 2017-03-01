0dc383dd1675fb299e15edf6f0808256b90c2f1e
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-03-01 09:43:45 +0000

    trying to add filter option

--- login/index.php
+++ login/index.php
@@ -13,17 +13,17 @@ include("auth.php");
     <title>Cosán Ceol</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="css1/bootstrap1.min.css" rel="stylesheet">
+    <link href="../css1/bootstrap1.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="css1/stylish-portfolio.css" rel="stylesheet">
+    <link href="../css1/stylish-portfolio.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="../font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
       
-      <link rel="stylesheet" type="text/css" href="comment_style.css">
-<script type="text/javascript" src="js/jquery.js"></script>
+      <link rel="stylesheet" type="text/css" href="../comment_style.css">
+<script type="text/javascript" src="../js/jquery.js"></script>
 <script src="testAjax.js"></script>
     <style>
       /* Always set the map height explicitly to define the size of the div
