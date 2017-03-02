3eb345318809f8c7bf1ccef6203b8518d61f9d02
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-03-02 17:44:04 +0000

    bug fixes

--- login/registration.php
+++ login/registration.php
@@ -27,32 +27,6 @@
   <span class="glyphicon glyphicon-home" aria-hidden="true"><a href="../index.php"> Home</a></span>
 </button>
  <a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle"><i class="fa fa-bars"></i></a>
-    <nav id="sidebar-wrapper">
-        <ul class="sidebar-nav">
-            <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle"><i class="fa fa-times"></i></a>
-            <li class="sidebar-brand">
-                <a href="#top" onclick=$("#menu-close").click();>Menu </a>
-            </li>
-            <li>
-                <a href="#top" onclick=$("#menu-close").click();>Home</a>
-            </li>
-            <li>
-                <a href="#about" onclick=$("#menu-close").click();>About</a>
-            </li>
-            <li>
-                <a href="login/logout.php" onclick=$("#menu-close").click();>Log out</a>
-            </li>
-            <li>
-                <a href="#map" onclick=$("#menu-close").click();>Search the Map</a>
-            </li>
-            <li>
-                <a href="#comment-section" onclick=$("#menu-close").click();>Leave a Comment</a>
-            </li>
-            <li>
-                <a href="#bottom" onclick=$("#menu-close").click();>Contact</a>
-            </li>
-        </ul>
-    </nav>
   <body onload="load()">
 <?php
 require('db.php');
