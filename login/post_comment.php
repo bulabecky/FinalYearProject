a3b9a5025c992905dffc135651160b1d6b819cc3
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-03-02 12:50:16 +0000

    adding sessions

--- login/post_comment.php
+++ login/post_comment.php
@@ -13,8 +13,10 @@ $con = mysqli_connect("localhost","root","Beckyboo4","register");
       $date = date("Y-m-d H:i:s");
 
 
-      $name = $_POST['user_name'];
-      $comment = $_POST['user_comm'];
+      $name = stripslashes($_POST['user_name']);
+      $name = mysqli_real_escape_string($con,$name);
+      $comment = stripslashes($_POST['user_comm']);
+      $comment = mysqli_real_escape_string($con,$comment);
 
       $insert = mysqli_query($con,"INSERT INTO comments(name, comment, post_time) VALUES ('$name','$comment', '$date')");
 
