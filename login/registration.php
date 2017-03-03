3429b63bb84f1d263d85c9a9d4fc102e2d129837
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-03-03 18:13:49 +0000

    username already exists

--- login/registration.php
+++ login/registration.php
@@ -31,14 +31,14 @@ require('db.php');
 // If form submitted, insert values into the database.
 if (isset($_REQUEST['username'])){
         // removes backslashes
-    $username = stripslashes($_REQUEST['username']);
+	$username = stripslashes($_REQUEST['username']);
         //escapes special characters in a string
-    $username = mysqli_real_escape_string($con,$username); 
-    $email = stripslashes($_REQUEST['email']);
-    $email = mysqli_real_escape_string($con,$email);
-    $password = stripslashes($_REQUEST['password']);
-    $password = mysqli_real_escape_string($con,$password);
-    $trn_date = date("Y-m-d H:i:s");
+	$username = mysqli_real_escape_string($con,$username); 
+	$email = stripslashes($_REQUEST['email']);
+	$email = mysqli_real_escape_string($con,$email);
+	$password = stripslashes($_REQUEST['password']);
+	$password = mysqli_real_escape_string($con,$password);
+	$trn_date = date("Y-m-d H:i:s");
     $query = "INSERT into `users` (username, password, email, trn_date)
 VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
         $result = mysqli_query($con,$query);
