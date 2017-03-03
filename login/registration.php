11f2a7141df984c5f89909e2417622aaafbf39f0
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-03-03 17:36:34 +0000

    username already exists

--- login/registration.php
+++ login/registration.php
@@ -39,19 +39,15 @@ if (isset($_REQUEST['username'])){
 	$password = stripslashes($_REQUEST['password']);
 	$password = mysqli_real_escape_string($con,$password);
 	$trn_date = date("Y-m-d H:i:s");
-        $query = "SELECT username FROM users WHERE username = '".$_POST['username']."'";
-        $query1= "INSERT into `users` (username, password, email, trn_date)
+        $query = "INSERT into `users` (username, password, email, trn_date)
 VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
-        //$result = mysqli_query($con,$query);
-        if(mysqli_num_rows($query) != 0){
-            echo "<h3> This username is takedn</h3";
-        }
-    }else{
-       
-$res = mysqli_query($con,$query1);
- echo "<div class='form'>
+        $result = mysqli_query($con,$query);
+        if($result){
+            echo "<div class='form'>
 <h3>You are registered successfully.</h3>
 <br/>Click here to <a href='login.php'>Login</a></div>";
+        }
+    }else{
 ?>
 <div class="form">
 <h1 style="color:white;">Registration</h1>
