8e0012e954424e12fdd676313dfe0aeb19cf7bf9
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-03-03 17:35:32 +0000

    username already exists

--- login/registration.php
+++ login/registration.php
@@ -40,13 +40,14 @@ if (isset($_REQUEST['username'])){
 	$password = mysqli_real_escape_string($con,$password);
 	$trn_date = date("Y-m-d H:i:s");
         $query = "SELECT username FROM users WHERE username = '".$_POST['username']."'";
+        $query1= "INSERT into `users` (username, password, email, trn_date)
+VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
         //$result = mysqli_query($con,$query);
         if(mysqli_num_rows($query) != 0){
             echo "<h3> This username is takedn</h3";
         }
     }else{
-       $query1= "INSERT into `users` (username, password, email, trn_date)
-VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
+       
 $res = mysqli_query($con,$query1);
  echo "<div class='form'>
 <h3>You are registered successfully.</h3>
