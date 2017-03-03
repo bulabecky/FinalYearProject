32ce0320d227e1cd41ea85880e24311275621018
Rebecca Ann Mc Gowan <REBECCA.MCGOWAN.2014@mumail.ie>
2017-03-03 18:00:22 +0000

    username already exists

--- login/registration.php
+++ login/registration.php
@@ -27,19 +27,31 @@
 </button> 
   <body onload="load()">
 <?php
+
 require('db.php');
 // If form submitted, insert values into the database.
 if (isset($_REQUEST['username'])){
         // removes backslashes
-	$username = stripslashes($_REQUEST['username']);
+    $username = stripslashes($_REQUEST['username']);
         //escapes special characters in a string
-	$username = mysqli_real_escape_string($con,$username); 
-	$email = stripslashes($_REQUEST['email']);
-	$email = mysqli_real_escape_string($con,$email);
-	$password = stripslashes($_REQUEST['password']);
-	$password = mysqli_real_escape_string($con,$password);
-	$trn_date = date("Y-m-d H:i:s");
-        $query = "INSERT into `users` (username, password, email, trn_date)
+    $username = mysqli_real_escape_string($con,$username); 
+    $email = stripslashes($_REQUEST['email']);
+    $email = mysqli_real_escape_string($con,$email);
+    $password = stripslashes($_REQUEST['password']);
+    $password = mysqli_real_escape_string($con,$password);
+    $trn_date = date("Y-m-d H:i:s");
+
+
+    $query = mysql_query("SELECT username FROM users WHERE username='$username'");
+
+  if (mysql_num_rows($query) != 0)
+  {
+      echo "Username already exists";
+  }
+
+  else
+  {
+     $query = "INSERT into `users` (username, password, email, trn_date)
 VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
         $result = mysqli_query($con,$query);
         if($result){
@@ -47,11 +59,14 @@ VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
 <h3>You are registered successfully.</h3>
 <br/>Click here to <a href='login.php'>Login</a></div>";
         }
-    }else{
+  }
+
+
+    }
 ?>
 <div class="form">
 <h1 style="color:white;">Registration</h1>
-<form name="registration" action="" method="post" id=formI>
+<form name="registration" action="" method="post" id=formID>
 <input type="text" name="username" placeholder="Username" required />
 <input type="email" name="email" placeholder="Email" required />
 <input type="password" name="password" placeholder="Password" required />
